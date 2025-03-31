b25005546aa0fa1b7d96c0d463542b85f3d2836f
AntoniIwan <245819@edu.p.lodz.pl>
2025-03-31 13:31:38 +0200

    added project blueprint to make next changes and writting code

---
+++ client/client.py
@@ -0,0 +1,14 @@
+if __name__ == '__main__':
+    pass
+    #connect to the server
+    #log to the server if first try just create account
+    #use system
+    #SPECIFICATION
+    # - logging
+    # - adding new taks lists
+    # - adding new tasks to choosen list
+    # - sorting using priority, expected time, time_of_adding
+    # - use genetic neural network to choose the best order of execution
+    # - ask for weather
+    # - ask for a joke
+    # - quck back to main list
