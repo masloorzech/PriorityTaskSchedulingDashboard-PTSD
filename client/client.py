52e0fa62ca15d07b56b2733033209cfe96c0ccbd
AntoniIwan <245819@edu.p.lodz.pl>
2025-03-31 14:10:50 +0200

    Added user input to try again connecting to server and colors to display

--- client/client.py
+++ client/client.py
@@ -7,17 +7,35 @@ url = 'http://{}:{}'.format(SERVER_IP, SERVER_PORT)
 def connect() -> (int,str):
     try:
         response = requests.get(url)
-        return 0, "SUCCESS: Connection established"
+        return 0, "\033[92mSUCCESS:\033[0m Connection established"
     except requests.exceptions.ConnectionError:
-        return -1, "ERROR: Cannot connect to server"
+        return -1, "\033[91mERROR:\033[0m Cannot connect to server"
     except requests.exceptions.Timeout:
-        return -1, "ERROR: Timeout server is not responding"
+        return -1, "\033[91mERROR:\033[0m Timeout server is not responding"
     except requests.exceptions.RequestException as e:
-        return -1, "ERROR: Unknown error"
+        return -1, "\033[91mERROR:\033[0m Unknown error"
+
+def establish_connection() -> int:
+    while True:
+        print("\033[94mEstablishing connection to server\033[0m")
+        return_code, return_message = connect()
+        if return_code == 0:
+            break
+        else:
+            print(return_message)
+        user_input = str(input("Do you want to try again? Y/N\n"))
+        if user_input.capitalize() == "N":
+            break
+    return return_code
 
 
 if __name__ == '__main__':
-    print(connect())
+
+    return_code = establish_connection()
+    if return_code != 0:
+        print("Thanks for using this program")
+        exit(return_code)
+
     #if cannot connect end program
 
     #connect to the server
