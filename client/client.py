ff58d3b4990e850a5b4d88f28401d4dcf63b8aac
AntoniIwan <245819@edu.p.lodz.pl>
2025-03-31 14:25:16 +0200

    added placeholder to loging and registering users

--- client/client.py
+++ client/client.py
@@ -4,7 +4,7 @@ SERVER_IP = '127.0.0.1'
 SERVER_PORT = 5000
 url = 'http://{}:{}'.format(SERVER_IP, SERVER_PORT)
 
-def connect() -> (int,str):
+def connect() -> tuple[int,str]:
     try:
         response = requests.get(url)
         return 0, "\033[92mSUCCESS:\033[0m Connection established"
@@ -19,24 +19,39 @@ def establish_connection() -> int:
     while True:
         print("\033[94mEstablishing connection to server\033[0m")
         return_code, return_message = connect()
+
+        print(return_message)
         if return_code == 0:
             break
-        else:
-            print(return_message)
-        user_input = str(input("Do you want to try again? Y/N\n"))
-        if user_input.capitalize() == "N":
-            break
+
+        user_input = input("Do you want to try again? Y/N\n").strip().upper()
+
+        if user_input == "N":
+            exit()
+
     return return_code
+
+def perform_logging() -> None:
+    print("Welcome to \033[1;97mTASK SYSTEM 2.0\033[0m\nPlease log in (command: \033[1;92m-log\033[0m), register (command: \033[1;92m-reg\033[0m) or quit (command: \033[1;92m-q\033[0m)")
+    while True:
+        user_input = input(">").strip().upper()
+        if user_input == "-LOG_IN":
+            pass
+        elif user_input == "-REGISTER":
+            pass
+        elif user_input == "-Q":
+            exit()
+        else:
+            print("\033[91mERROR:\033[0m Unknown command")
 
 
 if __name__ == '__main__':
 
-    return_code = establish_connection()
-    if return_code != 0:
-        print("Thanks for using this program")
-        exit(return_code)
+    perform_logging()
 
-    #if cannot connect end program
+    returned_value = establish_connection()
+
+
 
     #connect to the server
     #log to the server if first try just create account
