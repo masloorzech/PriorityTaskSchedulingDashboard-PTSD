6ddd04d05a670d7e81929bd605c3f2b14eae2307
AntoniIwan <245819@edu.p.lodz.pl>
2025-03-31 14:00:16 +0200

    Added connection try and catch to handle error while connecting to the server

--- client/client.py
+++ client/client.py
@@ -4,12 +4,22 @@ SERVER_IP = '127.0.0.1'
 SERVER_PORT = 5000
 url = 'http://{}:{}'.format(SERVER_IP, SERVER_PORT)
 
-def connect():
-    response = requests.get(url)
-    print(response.text)
+def connect() -> (int,str):
+    try:
+        response = requests.get(url)
+        return 0, "SUCCESS: Connection established"
+    except requests.exceptions.ConnectionError:
+        return -1, "ERROR: Cannot connect to server"
+    except requests.exceptions.Timeout:
+        return -1, "ERROR: Timeout server is not responding"
+    except requests.exceptions.RequestException as e:
+        return -1, "ERROR: Unknown error"
+
 
 if __name__ == '__main__':
-    connect()
+    print(connect())
+    #if cannot connect end program
+
     #connect to the server
     #log to the server if first try just create account
     #use system
