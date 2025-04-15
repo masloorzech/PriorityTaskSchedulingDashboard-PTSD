a37e94f1fb9af4cd0c42bdc94754cb9808873ac3
AntoniIwan <antoniiwan02@gmail.com>
2025-04-15 22:54:58 +0200

    Added weather API, gitignore and client functions for handling new features

--- server/routes/__init__.py
+++ server/routes/__init__.py
@@ -3,8 +3,8 @@
 def register_blueprints(app):
     from .users import users_bp
     from .tasks import tasks_bp
-    # from .weather import weather_bp
+    from .weather import weather_bp
 
     app.register_blueprint(users_bp, url_prefix="/users")
     app.register_blueprint(tasks_bp, url_prefix="/tasks")
-    #app.register_blueprint(weather_bp, url_prefix="/weather")
+    app.register_blueprint(weather_bp, url_prefix="/weather")
